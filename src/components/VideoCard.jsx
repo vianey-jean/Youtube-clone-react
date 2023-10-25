919a9a37e37f3764076b6f7a66b7e606976dfddd
vianey-jean <vianey.jean@ymail.com>
2023-10-25 09:50:33 +0400

    Correction des bugs

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,7 +9,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "400px", }, boxShadow: "none", borderRadius: "15px" , height:{md :"260px"}}}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+        sx={{ width: { xs: '100%', sm: '358px', md:"400px"}, height: 180 }} 
       />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
